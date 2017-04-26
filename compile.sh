68d3c46a20083a8b7c4ed7e1bfc5ca92e5bd12c4
snwfdhmp <snwfdhmp@protonmail.com>
2017-04-27 00:49:09 +0200

    Organizing and cleaning files

--- compile.sh
+++ compile.sh
@@ -4,10 +4,10 @@ version=$(cat config/compile_version_base)
 suffix=$(cat config/compile_version_suffix)
 suffix=$(($suffix+1))
 
-echo "Compilation de 'build/release-$version.$suffix.exe' ..."
+echo "[$(date)] ++ Compilation de 'build/release-$version.$suffix.exe' ..." >> logs/compile
 
-gcc src/main.cpp src/classes/*/*.cpp -o build/release-$version.$suffix.exe
+gcc src/main.cpp src/classes/*/*.cpp -o build/release-$version.$suffix.exe 
 
-git add * > /dev/null 2> /dev/null
+git add logs/compile > /dev/null 2> /dev/null
 git commit -m "[AUTO]Compilation de 'build/release-$version.$suffix.exe' ..." > /dev/null 2> /dev/null
 git push > /dev/null 2> /dev/null
