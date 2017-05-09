0b4b22ea2e57f616d9be0d772f764056ffa38dc0
snwfdhmp <snwfdhmp@protonmail.com>
2017-05-09 22:39:31 +0200

    duck class create Plateau

---
+++ src/classes/Plateau/Plateau.class.cpp
@@ -0,0 +1,15 @@
+// Project MPI [duck managed]
+// Class Plateau (src/classes/Plateau/Plateau.class.cpp)
+   #ifndef PLATEAU_CLASS_CPP
+    #define PLATEAU_CLASS_CPP
+
+    //Plateau class methods implementation
+
+    #include "Plateau.class.h"
+
+    //class constructor
+    Plateau::Plateau() {
+        // object initialization
+    }
+
+    #endif
---
+++ src/classes/Plateau/Plateau.class.h
@@ -0,0 +1,16 @@
+// Project MPI [duck managed]
+// Class Plateau (src/classes/Plateau/Plateau.class.h)
+#ifndef PLATEAU_CLASS_H
+    #define PLATEAU_CLASS_H
+    
+    //Plateau class definition
+
+    class Plateau
+    {
+        public:
+        Plateau(); //class constructor
+        ~Plateau();
+        
+    };
+
+    #endif
